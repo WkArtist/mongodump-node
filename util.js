071d7a343dedbd23f0617ddf0b224458f0844aa4
WkArtist <2550115859@qq.com>
2021-03-05 20:02:24 +0800

    添加工具函数

--- util.js
+++ util.js
@@ -1,95 +1,18 @@
-D:\back_end_job\360-score\mongodump-node
-├─.gitignore
-├─app.js
-├─config.js
-├─cronFunc.js
-├─init.js
-├─mongodump.js
-├─package-lock.json
-├─package.json
-├─README.md
-├─server.json
-├─sslProxy.js
-├─util.js
-├─dump
-├─.git
-|  ├─COMMIT_EDITMSG
-|  ├─config
-|  ├─description
-|  ├─FETCH_HEAD
-|  ├─HEAD
-|  ├─index
-|  ├─ORIG_HEAD
-|  ├─refs
-|  |  ├─tags
-|  |  ├─remotes
-|  |  |    ├─origin
-|  |  |    |   └master
-|  |  ├─heads
-|  |  |   └master
-|  ├─objects
-|  |    ├─pack
-|  |    ├─info
-|  |    ├─f8
-|  |    | └6f834f15768b0e53e95284901b8f45c3430257
-|  |    ├─eb
-|  |    | └08721b9909b756b9319056d84435fe0938fbf0
-|  |    ├─ea
-|  |    | └656d2b1706cfe12bacb2221695b381d617a0e0
-|  |    ├─e3
-|  |    | └9ecc8fdecea8890fccb55baf1c4c685206b274
-|  |    ├─de
-|  |    | └4bcaa775a506b727837d77f50ad91d39c55a7c
-|  |    ├─ba
-|  |    | └e97b7dbd47bd4b331b2130f411a9ed1fb41926
-|  |    ├─a5
-|  |    | └4bed02dba867c2a798a93d6bbfda6908de1344
-|  |    ├─9d
-|  |    | └a023f9b1a18530a9a814f685336e6748cd5734
-|  |    ├─99
-|  |    | └58d3d242e11cbefda422dede8fb59059e96c02
-|  |    ├─8f
-|  |    | └37a4b12c717d93c495bd517409f94a02d6279b
-|  |    ├─84
-|  |    | └6d64a647ace09207ae01abd67fccdc50343405
-|  |    ├─7e
-|  |    | └fe5782cc4644a4d006dd62468e2cbf656367ff
-|  |    ├─6e
-|  |    | └e34353a0eee2d3743426fdf50ad458eee3b593
-|  |    ├─54
-|  |    | └0e4aac9913e09a54373b903b429882e688b01f
-|  |    ├─34
-|  |    | └c835fc1b960562dac40c9e0e90b22e9c0ddd26
-|  |    ├─2e
-|  |    | └18c93a4aedd98c74a057fafbff9fb632246ecf
-|  |    ├─16
-|  |    | └ff9ba88435d1fe1e4e896bd3ecbb9d96d4b023
-|  |    ├─12
-|  |    | └49796017f04d8620fcb2802ef96b0fcc70bdc7
-|  |    ├─11
-|  |    | └4c0e0959a319bb2110cf86d75de512596f5ae6
-|  |    ├─0c
-|  |    | └0cec0ea04b7566abf01d4e78caaef0a41182ee
-|  ├─logs
-|  |  ├─HEAD
-|  |  ├─refs
-|  |  |  ├─remotes
-|  |  |  |    ├─origin
-|  |  |  |    |   └master
-|  |  |  ├─heads
-|  |  |  |   └master
-|  ├─info
-|  |  └exclude
-|  ├─hooks
-|  |   ├─applypatch-msg.sample
-|  |   ├─commit-msg.sample
-|  |   ├─fsmonitor-watchman.sample
-|  |   ├─post-update.sample
-|  |   ├─pre-applypatch.sample
-|  |   ├─pre-commit.sample
-|  |   ├─pre-merge-commit.sample
-|  |   ├─pre-push.sample
-|  |   ├─pre-rebase.sample
-|  |   ├─pre-receive.sample
-|  |   ├─prepare-commit-msg.sample
-|  |   └update.sample
+const cmd = require('node-cmd');
+
+let fun = {}
+
+// promise化 node-cmd
+fun.cmdPromise = async (line) => {
+    return new Promise((resolve, reject) => {
+        cmd.get(line, (err, data, stderr) => {
+            if (!err){
+                resolve(data);
+            } else {
+                reject(err);
+            }
+        });
+    });
+};
+
+module.exports = fun
